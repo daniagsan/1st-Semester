d7c42ade7c78425a33fdca11cef78fd9e2a20db5
daniagsan <jdas_9920@hotmail.com>
2022-10-02 16:50:24 -0600

    Extra work integration

---
+++ Extra work/18_extra.py
@@ -0,0 +1,20 @@
+#18.-Una agencia de autos necesita un sistema con lo necesario para 
+#calcular el pago mensual para un agente de ventas de autos, 
+#basándose en lo siguiente:
+
+#El pago base es de $350, más una comisión de $15 por cada 
+#auto vendido y un bono de $40 si vendió más de 15 autos.
+
+#El impuesto a pagar es el 5% del pago total.
+
+#Los datos de entrada son el nombre del vendedor y el número de 
+#autos vendidos en el mes.
+
+#Se desea imprimir el nombre del vendedor, el sueldo bruto, 
+#el impuesto y el sueldo neto.
+
+pago_base =  350
+comision = 15
+bono = 40
+impuesto = 0.5
+nombre =  []
